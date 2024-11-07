2411b2b118f74a93dd188f9a1318c11138ed15f8
hgamboa <hgamboa@polpulsa.com>
2024-11-07 17:21:26 -0600

    Conexion implementacion de Identity

---
+++ Domain/BaseEntity/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.BaseEntity
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int CreatedByUserId { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int UpdatedByUserId { get; set; }
+        public DateTime DeleteAt { get; set; } = DateTime.UtcNow;
+        public int DeletedByUserId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser,IdentityRole,string, IdentityUserClaim<string>,
+        IdentityUserRole<string>, IdentityUserLogin<string>,IdentityRoleClaim<string>,IdentityUserToken<string>>
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+    }
+}
