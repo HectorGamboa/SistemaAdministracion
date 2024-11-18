f6333a324f9e3ebd71d41b7dbc7cfa6fe62024b4
hector.gamboarejon.06031.15 <hector.gamboarejon.06031.15@gmail.com>
2024-11-18 17:57:59 -0600

    Construccion de primeros controladores

---
+++ Common/Responses/Identity/RoleResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Responses.Identity
+{
+    public class RoleResponse
+    {
+        public string ?Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- Infrastructure/ServiceCollectionExtensions.cs
+++ Infrastructure/ServiceCollectionExtensions.cs
@@ -1,7 +1,12 @@
-﻿using Infrastructure.Context;
+﻿
+using Application.Services.Identity;
+using Infrastructure.Context;
+using Infrastructure.Services.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
 
 namespace Infrastructure
 {
@@ -15,5 +20,23 @@ namespace Infrastructure
                 .AddTransient<ApplicationDBSeeder>();
             return services;
         }
+
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient<ITokenService, TokenService>()
+                .AddTransient<IUserService, UserService>()
+                .AddTransient<IRoleService, RoleService>()
+                //.AddHttpContextAccessor()
+                .AddScoped<ICurrentUserService, CurrentUserService>();
+
+            return services;
+        }
+
+        public static void AddInfrastructureDependencies(this IServiceCollection services)
+        {
+          services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
